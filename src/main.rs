e317a7b6f32aa494b4c547fa88baa2af7feb9e2e
haminem <ryuu.shushu@gmail.com>
2022-11-20 15:25:58 +0900

    add ファイルデータの読み込み/標準入出力の追加

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use std::env;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let filename = &args[1];
+
+    let f = File::open(filename).expect("ファイルが見つかりません");
+    let reader = BufReader::new(f);
+    for line in reader.lines() {
+        let line: String = line.unwrap();
+        let data: Vec<&str> = line.split(',').collect();
+        println!("{} {} {}", data[0], data[1], data[2]);
+    }
 }
