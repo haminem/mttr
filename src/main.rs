5647a1aaec8c946a25518742f4b366b20387a2b7
haminem <ryuu.shushu@gmail.com>
2022-11-21 00:09:43 +0900

    fix 二回目以降の故障期間をできるように修正

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,7 @@ fn main() {
     let response_time_average_capacity: u32 = args[4].parse().unwrap();
 
     let mut map_timeout: HashMap<String, (String, u32)> = HashMap::new();
-    let mut map_response_time: HashMap<String, (String, u32, VecDeque<u32>)> = HashMap::new();
+    let mut map_response_time: HashMap<String, (String, u32, VecDeque<u32>, bool, String)> = HashMap::new();
 
     let mut log1: String = String::new();
     let mut log2: String = String::new();
@@ -48,26 +48,44 @@ fn main() {
                     }
                     value.1 = 0;
                 });
-                map_response_time
-                .entry(ip_address.to_string())
-                .and_modify(|value: &mut (String, u32, VecDeque<u32>)| {
-                    value.1 += 1;
-                    //TODO: 冗長さの修正
-                    value.2.push_back(response_time.parse().unwrap());
-                    if value.1 >= response_time_average_range {
-                        if value.1 > response_time_average_range {
-                            value.2.pop_front();
-                        }
-                        let sum: u32 = value.2.iter().sum();
-                        let average: u32 = sum / response_time_average_range;
-                        if average >= response_time_average_capacity {
-                            log2.push_str(&format!("{} {} {}\n", ip_address, value.0, check_date));
-                        }
+            //TODO: response_time_average_rangeが1の場合の処理
+            map_response_time
+            .entry(ip_address.to_string())
+                .and_modify(|value: &mut (String, u32, VecDeque<u32>, bool, String)| {
+                value.1 += 1;
+                //TODO: 冗長さの修正
+                value.2.push_back(response_time.parse().unwrap());
+                if value.1 >= response_time_average_range {
+                    if value.1 > response_time_average_range {
+                        value.2.pop_front();
                     }
-                })
-                .or_insert((check_date.to_string(), 1, VecDeque::from([response_time.parse().unwrap()])));
+                    let sum: u32 = value.2.iter().sum();
+                    println!("sum: {}", sum);
+                    let average: u32 = sum / response_time_average_range;
+                    if average >= response_time_average_capacity {
+                        if !value.3 {
+                            value.0 = value.4.clone();
+                        }
+                        value.3 = true;
+                    }
+                    if average < response_time_average_capacity && value.3 {
+                        log2.push_str(&format!("{} {}~{}\n", ip_address, value.0, value.4));
+                        value.3 = false;
+                    }
+                    value.4 = check_date.to_string();
+                    println!("bool {}", value.3);
+                }
+            })
+                .or_insert((check_date.to_string(), 1, VecDeque::from([response_time.parse().unwrap()]), false, check_date.to_string()));
         }
     }
+    map_response_time
+        .iter()
+        .for_each(|(key, value)| {
+            if value.3 {
+                log2.push_str(&format!("{} {}~{}\n", key, value.0, value.4));
+            }
+        });
     println!("{:?}", map_timeout);
     println!("\n");
     println!("{}", log1);
