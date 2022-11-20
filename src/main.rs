9228532d0629cffbe0f76033b766184757658c88
haminem <ryuu.shushu@gmail.com>
2022-11-20 20:14:24 +0900

    add サーバーの故障判定に冗長性を追加

--- src/main.rs
+++ src/main.rs
@@ -6,9 +6,10 @@ use std::collections::HashMap;
 fn main() {
     let args: Vec<String> = env::args().collect();
     let filename: &String = &args[1];
+    let redundant: u32 = args[2].parse().unwrap();
 
     let mut map = HashMap::new();
-    let mut log = String::new();
+    let mut log1 = String::new();
 
     let f = File::open(filename).expect("ファイルが見つかりません");
     let reader = BufReader::new(f);
@@ -24,23 +25,33 @@ fn main() {
         let response_time: &str = data[2];
         println!("{} {} {}", check_date, ip_address, response_time);
 
+        //TODO: 条件分岐の冗長性を減らす
         if response_time == "-" {
+            if map.contains_key(ip_address) {
+                let value: &mut (String, u32) = map.get_mut(ip_address).unwrap();
+                value.1 += 1;
+                if value.1 >= redundant {
+                    log1.push_str(&format!("{} {} {}\n", ip_address, value.0, check_date));
+                    map.remove(ip_address);
+                }
+            } else {
             //add data to map
             let key: String = ip_address.to_string();
-            let value: String = check_date.to_string();
+            let value: (String, u32) = (check_date.to_string(), 1);
             map.entry(key).or_insert(value);
-        } else {
-            for (key, value) in &map {
-                if key == ip_address {
-                    log.push_str(&format!("{} {}~{}\n", ip_address, value, check_date));
-                }
             }
-            //remove data from map
-            map.remove(ip_address);
+        } else {
+            if map.contains_key(ip_address) {
+                let value: &mut (String, u32) = map.get_mut(ip_address).unwrap();
+                if value.1 >= redundant {
+                    log1.push_str(&format!("{} {} {}\n", ip_address, value.0, check_date));
+                }
+                map.remove(ip_address);
+            }
         }
     }
     println!("{:?}", map);
-    println!("{}", log);
+    println!("{}", log1);
     //write to log1.txt
-    log1_file.write_all(log.as_bytes()).expect("ファイルに書き込めません");
+    log1_file.write_all(log1.as_bytes()).expect("ログファイル1に書き込めません");
 }
